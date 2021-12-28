d945c1796259a5cb085d6bd565a2886f95eeeb59
Stas Elovsky <stas.elvosky@gmail.com>
2021-12-28 21:46:05 +0300

    add navigation

--- app/src/main/java/com/helicoptera/pixures/MainActivity.kt
+++ app/src/main/java/com/helicoptera/pixures/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.helicoptera.pixures
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+
+        if (navHostFragment != null) {
+            val navController = navHostFragment.navController
+
+            NavigationUI.setupWithNavController(bottomNavigation, navController)
+        }
     }
 }
